12335877a667add66980f67144f763ec1cfa4287
viniciustneiva <viniciustneiva@gmail.com>
2022-02-26 01:04:02 -0300

    alteracoes

--- src/Tabela.java
+++ src/Tabela.java
@@ -3,16 +3,15 @@ import java.util.*;
 public class Tabela {
 	private String[][] tabela;
 	private String[] elementos = {" ", "1", "2", "3", "4", "5", "6", "7", "8"};
-	private String[] resultado = {"1", "2", "3", "8", " ", "4", "7", "6", "5"}; 
+	private String[] resultado = {"1", "2", "3", "8", " ", "4", "7", "6", "5"};
+	
 	public static void embaralhar(String [] v) {
 
 		Random random = new Random();
 
 		for (int i=0; i < (v.length - 1); i++) {
-
 			//sorteia um índice
 			int j = random.nextInt(v.length);
-
 			//troca o conteúdo dos índices i e j do vetor
 			String temp = v[i];
 			v[i] = v[j];
@@ -27,7 +26,6 @@ public class Tabela {
 		int k = 0;
 		for(int linha = 0; linha < 3 && k<9; linha++) {
 			for(int coluna = 0; coluna < 3 && k<9; coluna++) {
-
 				this.tabela[linha][coluna] = elementos[k];
 				k++;
 			}
@@ -39,7 +37,6 @@ public class Tabela {
 		for(int linha = 0; linha < 3; linha++) {
 			for(int coluna = 0; coluna < 3; coluna++) {
 				System.out.print(this.tabela[linha][coluna]+" ");
-			
 			}
 			System.out.println("");
 		}
